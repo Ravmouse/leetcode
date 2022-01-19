d37e745ab6d9b2a9978aa106d822ba74f1e1f79a
Vitaly Vasilyev <rav.energ@rambler.ru>
2022-01-19 19:13:01 +0300

    88. Merge Sorted Array

--- src/main/java/ru/vitaly/_88_MergeSortedArray.java
+++ src/main/java/ru/vitaly/_88_MergeSortedArray.java
@@ -3,12 +3,13 @@ package ru.vitaly;
 /**
  * @author Vitaly Vasilyev, e-mail: rav.energ@rambler.ru
  * @version 1.0
- * @since 17.09.2021
+ * @since 19.01.2022
  */
 public class _88_MergeSortedArray {
-
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1, j = n - 1, k = (m + n) - 1;
+        int i = m - 1;
+        int j = n - 1;
+        int k = (m + n) - 1;
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
                 nums1[k--] = nums1[i--];
@@ -16,7 +17,6 @@ public class _88_MergeSortedArray {
                 nums1[k--] = nums2[j--];
             }
         }
-        while (i >= 0) nums1[k--] = nums1[i--];
         while (j >= 0) nums1[k--] = nums2[j--];
     }
 }
--- src/test/java/ru/vitaly/_88_MergeSortedArrayTest.java
+++ src/test/java/ru/vitaly/_88_MergeSortedArrayTest.java
@@ -8,7 +8,7 @@ import static ru.vitaly._88_MergeSortedArray.merge;
 /**
  * @author Vitaly Vasilyev, e-mail: rav.energ@rambler.ru
  * @version 1.0
- * @since 17.09.2021
+ * @since 19.01.2022
  */
 public class _88_MergeSortedArrayTest {
     @Test
@@ -30,5 +30,26 @@ public class _88_MergeSortedArrayTest {
         int[] array = new int[] {0};
         merge(array, 0, new int[] {1}, 1);
         assertThat(array, is(new int[] {1}));
+    }
+
+    @Test
+    public void test4() {
+        int[] array = new int[] {1,3,7,10,15,0,0,0};
+        merge(array, 5, new int[] {2,3,11}, 3);
+        assertThat(array, is(new int[] {1,2,3,3,7,10,11,15}));
+    }
+
+    @Test
+    public void test5() {
+        int[] array = new int[] {1,3,7,10,15,0,0,0};
+        merge(array, 5, new int[] {8,11,16}, 3);
+        assertThat(array, is(new int[] {1,3,7,8,10,11,15,16}));
+    }
+
+    @Test
+    public void test6() {
+        int[] array = new int[] {10,15,0,0,0,0,0};
+        merge(array, 2, new int[] {6,9,11,14,16}, 5);
+        assertThat(array, is(new int[] {6,9,10,11,14,15,16}));
     }
 }
