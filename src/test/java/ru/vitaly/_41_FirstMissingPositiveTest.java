fdfdb08ce26a32b5553087bdd5ff6e01c005987a
Vitaly <rav.energ@rambler.ru>
2022-03-11 19:02:50 +0300

    41. First Missing Positive (some tests added)

--- src/test/java/ru/vitaly/_41_FirstMissingPositiveTest.java
+++ src/test/java/ru/vitaly/_41_FirstMissingPositiveTest.java
@@ -6,28 +6,42 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static ru.vitaly._41_FirstMissingPositive.firstMissingPositive;
 
 /**
- * @author Vitaly Vasilyev, date: 12.09.2021, e-mail: rav.energ@rambler.ru
+ * @author Vitaly Vasilyev, e-mail: rav.energ@rambler.ru
  * @version 1.0
- * @since 12.09.2021
  */
 public class _41_FirstMissingPositiveTest {
     @Test
-    public void test_1() {
+    public void test1() {
         assertThat(firstMissingPositive(new int[] {1,2,0}), is(3));
     }
 
     @Test
-    public void test_2() {
+    public void test2() {
         assertThat(firstMissingPositive(new int[] {3,4,-1,1}), is(2));
     }
 
     @Test
-    public void test_3() {
+    public void test3() {
         assertThat(firstMissingPositive(new int[] {7,8,9,11,12}), is(1));
     }
 
     @Test
-    public void test_4() {
+    public void test4() {
         assertThat(firstMissingPositive(new int[] {4,7,6,5,3,2}), is(1));
+    }
+
+    @Test
+    public void test5() {
+        assertThat(firstMissingPositive(new int[] {1}), is(2));
+    }
+
+    @Test
+    public void test6() {
+        assertThat(firstMissingPositive(new int[] {1, 1}), is(2));
+    }
+
+    @Test
+    public void test7() {
+        assertThat(firstMissingPositive(new int[] {2, 1}), is(3));
     }
 }
